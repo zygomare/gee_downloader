ccf2e7fe6a88af1504413e4ff5c482af867ef586
Simon Hurens <sh@arctus.ca>
2024-10-29 15:53:23 -0400

    Update iterator to target separate folder

--- iterator.py
+++ iterator.py
@@ -1,4 +1,5 @@
 import configparser
+from tqdm import tqdm
 config = configparser.ConfigParser()
 #config.sections()
 config.read("download.ini")
@@ -6,14 +7,18 @@ config.read("download.ini")
 # import required module
 import os
 # assign directory
-nonna = 'nonna_ROIs'   #folder of ROIs in the same directory
+nonna_dir = r'c:\Users\sim-9\MEI_SDB\nonna_ROIs'   #folder of ROIs in the same directory
+output_dir = r'c:\Users\sim-9\MEI_SDB\GEE_data_nonna'  #output folder where we download GEE images.
+
+if not os.path.exists(output_dir): os.makedirs(output_dir)
  
 # iterate over files in
 # that directory
 count = 0
-for filename in os.listdir(nonna):
-    aoi = os.path.join(nonna, filename)
-    output = "{}/{}".format('Results', count)   #name of folder to save results
+for filename in tqdm(os.listdir(nonna_dir)):
+    aoi = os.path.join(nonna_dir, filename)
+    print(f"Running GEE-downloader on AOI: {aoi}")
+    output = os.path.join(output_dir, f"aoi_{count}")   #name of folder to save results
     # checking if it is a file
     if os.path.isfile(aoi):
         config['GLOBAL']['aoi'] = aoi      #update aoi
@@ -21,5 +26,5 @@ for filename in os.listdir(nonna):
         with open("download.ini", "w") as file_object:
             config.write(file_object)         #saves changes
             
-    %run main.py -c download.ini
+    os.system("python main.py -c download.ini")
     count +=1           #counter. Folder names may not match aoi ID.
