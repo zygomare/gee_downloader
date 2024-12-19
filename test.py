19f825441f32e67ef27223b6c6ab7b6b43cf6ba5
thomas <zygomare@gmail.com>
2024-12-19 09:07:02 -0500

    add multiple new ini

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def test_merge_download_dir_obsgeo():
     from gee.utils import merge_download_dir_obsgeo
     from gee import get_obsgeo_s2_l1toa
     dst_crs = merge_download_dir_obsgeo(func_obsgeo=get_obsgeo_s2_l1toa,
-                              download_dir='E:/Intel-port-data/L1/s2_msi/2023-08-23',output_f='',descriptions='', descriptions_meta='')
+                              download_dir='/media/thomas/Arctus_data1/Data/0_ARCTUS_Projects/12_SmartEarth_BSI/data/smart_earth/L1/L1/s2_msi/2023-01-04',output_f='',descriptions='', descriptions_meta='')
     print(dst_crs)
 
 
